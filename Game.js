b296f5ff8b759a07838361161e67d7a8bdc1d90c
zegaimoumen <58592686+zegaimoumen@users.noreply.github.com>
2022-07-20 07:35:35 -0700

    Update Game.js

--- Game.js
+++ Game.js
@@ -59,7 +59,7 @@ function main(currentTime) {
   if (gameOver) {
     document.getElementById("game-over").style.visibility = "visible";
     replay.onclick = function () {
-      window.location.reload(true);
+      window.location = "https://zegaimoumen.github.io/snake-game/";
     };
 
     window.addEventListener("keyup", (e) => {
