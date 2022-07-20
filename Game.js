9708c129368059abfebb278122fc3e65a9349f0e
zegaimoumen <58592686+zegaimoumen@users.noreply.github.com>
2022-07-20 07:15:56 -0700

    Update Game.js

--- Game.js
+++ Game.js
@@ -59,7 +59,7 @@ function main(currentTime) {
   if (gameOver) {
     document.getElementById("game-over").style.visibility = "visible";
     replay.onclick = function () {
-      window.location.relode();
+      window.location.relode(true);
     };
 
     window.addEventListener("keyup", (e) => {
